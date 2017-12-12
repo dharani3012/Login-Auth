c28598676ea536ca5c4aba4750e087453d2ffdbc
kishorsnk <kishorsnk@gmail.com>
2017-12-12 11:16:11 +0530

    snk-auth-branch

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -4,13 +4,30 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  host: {
+        '(window:scroll)': 'updateHeader($event)'
+    }
 })
 export class HeaderComponent implements OnInit {
+
+	isScrolled = false;
+    currPos: Number = 0;
+    startPos: Number = 0;
+    changePos: Number = 100;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+
+  updateHeader(evt) {
+        this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+        if(this.currPos >= this.changePos ) {
+            this.isScrolled = true;
+        } else {
+            this.isScrolled = false;
+        }
+    }
 }
